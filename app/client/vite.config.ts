6e1d63df8a8a4d36a75ce85243d27fde8096d787
Aarass <prokopovic75@gmail.com>
2025-07-10 21:28:49 +0200

    tailwind, shadcn, ...

--- app/client/vite.config.ts
+++ app/client/vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from "vitest/config"
-import react from "@vitejs/plugin-react"
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    open: true,
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests",
-    mockReset: true,
-  },
-})
+});
