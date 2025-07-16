fcad4b03ad71077090c4fd0fa346b886287db0f8
Aarass <Prokopovic75@gmail.com>
2025-07-16 20:49:11 +0200

    prebac laptop na komp

--- app/client/src/features/leaderboards/SearchLeaderboard.tsx
+++ app/client/src/features/leaderboards/SearchLeaderboard.tsx
@@ -12,18 +12,19 @@ import { useEffect } from "react";
 import { Controller, useForm } from "react-hook-form";
 import { GenreAutocomplete } from "../genres/GenreAutocomplete";
 import { Genre } from "@interfaces/genre";
+import { useGetLeaderboardQuery } from "./leaderboardsApi";
 
 export function SearchLeaderboard() {
   const { register, handleSubmit, watch, control } = useForm();
 
   // useEffect(() => {
   //   const subscription = watch((value, { name, type }) => {
-  //     console.log("ovde");
   //     submit(value);
   //   });
   //
   //   return () => subscription.unsubscribe();
   // }, [watch]);
+  const tmp = useGetLeaderboardQuery();
 
   function submit(data: any) {
     const criteria: "rating" | "readers" = data["criteria"];
@@ -37,32 +38,31 @@ export function SearchLeaderboard() {
       onSubmit={handleSubmit(submit)}
       className="grid gap-4 p-4 grid-cols-[1fr_1fr]"
     >
-      <div className="flex flex-1 gap-4">
-        <div>
-          <Label htmlFor="criteria">Criteria</Label>
+      <div className="grid grid-cols-[max-content_max-content_min-content] grid-rows-[min-content_auto] gap-y-1 gap-x-4 items-center">
+        <Label htmlFor="criteria">Criteria</Label>
+        <Label htmlFor="genre">Genre</Label>
+        <div />
 
-          <Controller
-            defaultValue="rating"
-            control={control}
-            name="criteria"
-            rules={{
-              required: true,
-            }}
-            render={({ field }) => (
-              <Select defaultValue={field.value} onValueChange={field.onChange}>
-                <SelectTrigger className="w-[180px]" id="criteria">
-                  <SelectValue />
-                </SelectTrigger>
-                <SelectContent>
-                  <SelectItem value="rating">Best rated</SelectItem>
-                  <SelectItem value="readers">Most popular</SelectItem>
-                </SelectContent>
-              </Select>
-            )}
-          />
-        </div>
+        <Controller
+          defaultValue="rating"
+          control={control}
+          name="criteria"
+          rules={{
+            required: true,
+          }}
+          render={({ field }) => (
+            <Select defaultValue={field.value} onValueChange={field.onChange}>
+              <SelectTrigger className="w-[180px]" id="criteria">
+                <SelectValue />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="rating">Best rated</SelectItem>
+                <SelectItem value="readers">Most popular</SelectItem>
+              </SelectContent>
+            </Select>
+          )}
+        />
         <div className="w-[180px]">
-          <Label htmlFor="genre">Genre</Label>
           <GenreAutocomplete control={control} name="genre" />
         </div>
 
