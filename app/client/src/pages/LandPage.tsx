675e92233dd11f7651c54ab21eab8fe0be27704a
Aarass <prokopovic75@gmail.com>
2025-10-10 10:13:37 +0200

    pattern background

--- app/client/src/pages/LandPage.tsx
+++ app/client/src/pages/LandPage.tsx
@@ -3,17 +3,17 @@ import { Globe } from "lucide-react";
 
 export function LandPage() {
   return (
-    <div className="flex-1 flex flex-col items-center justify-center mb-40">
+    <div className="flex-1 flex flex-col items-center justify-center bgbooks after:bg-[#EEEEEE] dark:after:bg-[#111111]">
       <div className="flex items-end mb-6">
         <Globe size={150} />
         <h1 className="text-9xl font-bold">BookSphere</h1>
       </div>
 
-      <h2 className="text-4xl tracking-[0.45em] font-bold opacity-50">
+      <h2 className="text-4xl indent-5 tracking-[0.45em] font-bold opacity-50">
         Dive into a world of books
       </h2>
 
-      <Button className="mt-10">Get Started</Button>
+      <Button className="mb-40 mt-10">Get Started</Button>
     </div>
   );
 }
